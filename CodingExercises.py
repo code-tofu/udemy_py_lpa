530da99f99242cac5f32d9603554bd13c11d6ec6
benfoo268 <benjamin.ftc@gmail.com>
2022-04-17 14:24:40 +0800

    Exercises/Solutions Changes

--- CodingExercises.py
+++ CodingExercises.py
@@ -141,7 +141,55 @@
 #takes in user input as string of 3 numbers separated by commas
 #use loop to string each list item until the comma
 #convert each list item into int
-userstring = input("Please enter three numbers:")
-userlist = userstring.split(",")
-#split method splits a string into a list where each word is a list item
-print(int(userlist[0])+int(userlist[1])-int(userlist[2]))
+# userstring = input("Please enter three numbers:")
+# userlist = userstring.split(",")
+# #split method splits a string into a list where each word is a list item
+# print(int(userlist[0])+int(userlist[1])-int(userlist[2]))
+
+
+# # EXERCISE 15
+# albums = [
+#     ("Welcome to my Nightmare", "Alice Cooper", 1975,
+#      [
+#          (1, "Welcome to my Nightmare"),
+#          (2, "Devil's Food"),
+#          (3, "The Black Widow"),
+#          (4, "Some Folks"),
+#          (5, "Only Women Bleed"),
+#      ]
+#      ),
+#     ("Bad Company", "Bad Company", 1974,
+#      [
+#          (1, "Can't Get Enough"),
+#          (2, "Rock Steady"),
+#          (3, "Ready for Love"),
+#          (4, "Don't Let Me Down"),
+#          (5, "Bad Company"),
+#          (6, "The Way I Choose"),
+#          (7, "Movin' On"),
+#          (8, "Seagull"),
+#      ]
+#      ),
+#     ("Nightflight", "Budgie", 1981,
+#      [
+#          (1, "I Turned to Stone"),
+#          (2, "Keeping a Rendezvous"),
+#          (3, "Reaper of the Glory"),
+#          (4, "She Used Me Up"),
+#      ]
+#      ),
+#     ("More Mayhem", "Imelda May", 2011,
+#      [
+#          (1, "Pulling the Rug"),
+#          (2, "Psycho"),
+#          (3, "Mayhem"),
+#          (4, "Kentish Town Waltz"),
+#      ]
+#      ),
+# ]
+ 
+# # Add your code below this comment.
+# print(albums[1][3][5][1]) #the way i choose
+# print(albums[2][2]) #year of nightlight
+# print(albums[3][3][3][0]) #track number of kentish town waltz
+# print(albums[2][3][1]) #keeping a rendezvous tuple
