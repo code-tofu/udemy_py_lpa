7da70d0a63a8aafba03ab4b1020afb7a6d7ae8c7
Benjamin Foo <benjamin.ftc@gmail.com>
2024-07-13 20:30:58 +0800

    189

--- Notes/C7A.py
+++ Notes/C7A.py
@@ -1,4 +1,6 @@
-#diciontary litervehicles = { 'dream':'Honda250T',
+#diciontary literal
+
+vehicles = { 'dream':'Honda250T',
 'roadster':'BMW R1100',
 'virago':'Yamaha XV250'
 } # using {} instead of [] and consisting of a key:value pair
@@ -21,14 +23,16 @@ vehicles['tenere'] = 'Yamaha XT650'
 for key,value in vehicles.items(): #does not have to be named key
     print(key,value,sep="-")
 
-#updates items
+#updates items in dict
 vehicles['virago'] = 'Yamaha XV535'
 
 #delete, will throw error
 del vehicles['virago']
-del vehicles.pop('virago')
 
-#set pop to return none or an alternative if key is not found
-del vehicles.pop('virago',None)
+# set pop to return none or an alternative if key is not found
+result = vehicles.pop('virago',None)
+print(result)
 
+# pop will throw error
+vehicles.pop('virago')
 
--- Notes/C7B.py
+++ Notes/C7B.py
@@ -1,3 +1,12 @@
+# see 189abs
+
+#add list implementation
+
+## compare with list
+##. edit me 190 
+
+
+
 available_parts = {"1": "computer",
                    "2": "monitor",
                    "3": "keyboard",
@@ -6,25 +15,40 @@ available_parts = {"1": "computer",
                    "6": "dvd drive",
                    }
 
+
+
+##list implementation 
+# computer_parts = []  
+# available_parts.sort()
+
+
 current_choice = None
-computer_parts = []  # create an empty list
+computer_parts={}
 
 while current_choice != "0":
-    if current_choice in available_parts:
+    if current_choice in available_parts: #in only checks the keys in a dictionary
         chosen_part = available_parts[current_choice]
         if chosen_part in computer_parts:
             # it's already in, so remove it
             print(f"Removing {chosen_part}")
-            computer_parts.remove(chosen_part)
+            ## computer_parts.remove(chosen_part) #list implementation
+            computer_parts.pop(chosen_part)
         else:
             print(f"Adding {chosen_part}")
-            computer_parts.append(chosen_part)
-        print(f"Your list now contains: {computer_parts}")
+            ## computer_parts.append(chosen_part)
+            computer_parts[current_choice] = chosen_part
+            
+        ## print(f"Your list now contains: {computer_parts}")
+        print(f"Your dictionary now contains: {computer_parts}")
     else:
         print("Please add options from the list:")
-        for key, value in available_parts.items():
+        for key, value in available_parts.items(): # return all entries
             print(f"{key}: {value}")
         print("0: to finish")
 
     current_choice = input("> ")
 
+
+
+#191 
+# contents.txt
