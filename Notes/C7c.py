df2bbf4d6763b85d71d00fab04c2b05a0d674fee
Benjamin Foo <benjamin.ftc@gmail.com>
2024-11-06 00:35:30 +0800

    c7

--- Notes/C7c.py
+++ Notes/C7c.py
@@ -1,5 +1,5 @@
 # any python object can be a value in python 
 
-
-# dictionary comprehensions is more efficieng 
-# enumertae works with lists which returns a tuple 
+# dictionary comprehensions is more efficient
+# enumerate works with lists which returns a tuple with a numerated index
+# change to str so dont have to covert user input back
