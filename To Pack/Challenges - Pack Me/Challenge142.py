f2d1c79c69da7514ab4ce23e2f403c1b6ef631ba
benjaminfoo <benjamin.ftc@gmail.com>
2024-11-19 00:54:44 +0800

    Final Pack Onwards with L7

--- To Pack/Challenges - Pack Me/Challenge142.py
+++
@@ -1,9 +0,0 @@
-#CHALLENGE 142
-def palindrome_sentence(sentence):
-    string = ""
-    for char in sentence:
-        if char.isalnum():
-            string += char
-    print(string)
-    return string[::-1].casefold() == string.casefold()
-
